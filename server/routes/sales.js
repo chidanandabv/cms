73b7a0f93c16050434405c79215fe18b36478a0c
chidanandabv <chidanandabv23@gmail.com>
2024-07-21 06:07:00 +0530

    server side is completed

--- server/routes/sales.js
+++ server/routes/sales.js
@@ -1,8 +1,3 @@
 import express from "express";
-import { getSales } from "../controllers/sales.js";
-
-const router = express.Router();
-
-router.get("/sales", getSales);
-
+const router =express.Router();
 export default router;
